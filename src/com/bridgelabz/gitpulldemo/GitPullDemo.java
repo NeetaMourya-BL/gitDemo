3a719bf8139c8d97c398f05aa6b17a420dff2fed
NEETAMOURYA <neeta.mourya@bridgelabz.com>
2022-03-29 16:31:03 +0530

    first commit

---
+++ src/com/bridgelabz/gitpulldemo/GitPullDemo.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.gitpulldemo;
+
+public class GitPullDemo {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Java 1");
+		System.out.println("Hello Java 2");
+	}
+
+}
